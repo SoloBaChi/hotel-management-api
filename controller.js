c3f114675fe9933a532f3270c5b03b1b23f831e0
Joboson Solomon Chiakonam <solobachi02@gmail.com>
2023-02-12 22:05:55 +0100

    fresh update

--- controller.js
+++ controller.js
@@ -11,7 +11,7 @@ return new Promise((resolve,reject)=> resolve(roomData))
 async getRoom(id){
 return new Promise((resolve,reject)=>{
 //get room
-let room = roomData.find(todo => todo.id === parseInt(id));
+let room = roomData.find(item => item.id === parseInt(id));
 if(room){
  resolve(room)
 }
@@ -34,4 +34,25 @@ resolve(newRoom)
 }
 
 //update an existing room
+async updateRoom(id){
+return new Promise((resolve,reject)=>{
+//get the room
+let room = roomData.filter(item => item.id === parseInt(id));
+if(!room){
+reject(`The room with id ${id} does not exist!`)
+}
+resolve(room)
+})
+}
+
+//Delete an existing room
+async deleteRoom(id){
+return new Promise((resolve,reject)=>{
+let room = roomData.filter(item => item.id !== parseInt(id));
+if(!room){
+reject(`The room with id ${id} does not exist!`)
+}
+resolve(`room with the id ${id} has been succesfully deleted!`)
+})
+}
 }
